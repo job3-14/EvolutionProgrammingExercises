92740285c9bba6f57e85191562e1f02a2dc7ecd2
sakai <amu@sakai.pro>
2021-04-15 08:57:25 +0900

    Helloworld

---
+++ No1/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld { // <= クラス宣言の開始
+    public static void main(String[] args){ // <= mainメソッドの開始
+        System.out.println("Hello World");
+    } // <= mainメソッドの終了
+} // <= クラス宣言の終了
