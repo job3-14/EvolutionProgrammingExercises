a7e19e8235bddf3e356262014e9b7a1d6ba3571f
sakai <amu@sakai.pro>
2021-04-15 09:13:09 +0900

    完成

---
+++ No1/BigAndSmall.java
@@ -0,0 +1,13 @@
+public class BigAndSmall{
+  public static void main(String[] args){
+    Double value = Math.random();
+    // valueには0から1の乱数が代入されている．
+    System.out.printf("value: %f: ", value);
+
+    if(value <= 0.5){
+      System.out.println("Small");
+    }else{
+      System.out.println("Big");
+    }
+  }
+}
