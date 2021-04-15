b4e4d5de5c92baed8fc243b10c739389dc3b42b6
sakai <amu@sakai.pro>
2021-04-15 09:26:38 +0900

    処理追加

--- No1/leapyear.java
+++ No1/leapyear.java
@@ -2,7 +2,17 @@ public class LeapYear{
     public static void main(String[] args){
         Integer year = 2016;
         Boolean leapYear = false;
+        
         // ここに判定処理を書いていく．
+        if(year%4 == 0){
+          if(year%100 == 0){
+            if(year%400 == 0){
+              leapYear = true;
+            }
+          }else{
+            leapYear = true;
+          }
+        }
 
         if(leapYear){ // leapYearがtrueの場合．
             System.out.printf("%d年はうるう年です．%n", year);
