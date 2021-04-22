f0fda27cfc88c5cb4a8c41d778e2b1b1e8921e49
sakai <amu@sakai.pro>
2021-04-22 11:26:03 +0900

    ignore編集

---
+++ No2/ArgsSorter.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class ArgsSorter{
+  void run(String[] args){
+    // ここで，printArray を呼び出し，"before"の一行を出力する．
+
+    // argsの内容をソートするため，Arrays.sortメソッドを呼び出す．
+    Arrays.sort(args); // <= args がソート済みになる．
+    // ここで，printArray を呼び出し，"after"の一行を出力する．
+  }
+  // printArrayメソッドをここに書く．
+  void printArray(
+  
+  )
+  // mainメソッドは省略．
+}
