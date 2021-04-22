df272e1ae3c60c2c6a230c88daf93e0f53b25454
sakai <amu@sakai.pro>
2021-04-22 09:32:13 +0900

    年号判別追加

--- No2/SeirekiToWarekiConverter.java
+++ No2/SeirekiToWarekiConverter.java
@@ -1,7 +1,18 @@
 public class SeirekiToWarekiConverter{
   public static void main(String[] args){
+    Integer result;
     System.out.println(args[0]);
     Integer year = Integer.valueOf(args[0]);
-    System.out.printf("%d",year);
+    if(year < 1868){
+      result = -1;
+    }else if(year < 1912){
+      result = year - 1867;
+    }else if(year < 1926){
+      result = year - 1911;
+    }else if(year < 1989){
+      result = year - 1925;
+    }else if(year < 2019){
+      result = year - 2018;
+    }
   }
 }
