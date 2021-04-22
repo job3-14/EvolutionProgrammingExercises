7f7c3592ad5850e025e06b55fa550235fa8e277c
sakai <amu@sakai.pro>
2021-04-22 09:23:43 +0900

    入力作成

---
+++ No2/SeirekiToWarekiConverter.java
@@ -0,0 +1,7 @@
+public class SeirekiToWarekiConverter{
+  public static void main(String[] args){
+    System.out.println(args[0]);
+    Integer year = Integer.valueOf(args[0]);
+    System.out.printf("%d",year);
+  }
+}
