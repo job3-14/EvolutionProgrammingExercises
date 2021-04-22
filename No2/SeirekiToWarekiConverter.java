e796ddfc7fcae5362d52f4e039b1580e6fd8745d
sakai <amu@sakai.pro>
2021-04-22 09:42:59 +0900

    基本出力追加

--- No2/SeirekiToWarekiConverter.java
+++ No2/SeirekiToWarekiConverter.java
@@ -3,17 +3,24 @@ public class SeirekiToWarekiConverter{
     Integer result;
     //System.out.println(args[0]);
     Integer year = Integer.valueOf(args[0]);
-    System.out.printf("西暦:%d",year);
+    System.out.printf("西暦%d: ",year);
     if(year < 1868){
       result = -1;
     }else if(year < 1912){
       result = year - 1867;
+      System.out.printf("明治%d年%n",result);
     }else if(year < 1926){
       result = year - 1911;
+      System.out.printf("大正%d年%n",result);
     }else if(year < 1989){
       result = year - 1925;
+      System.out.printf("昭和%d年%n",result);
     }else if(year < 2019){
+      result = year - 1988;
+      System.out.printf("平成%d年%n",result);
+    }else{
       result = year - 2018;
+      System.out.printf("令和%d年%n",result);
     }
   }
 }
