6b53270f86346215ae2f31900f1835d45eb3a97d
sakai-desk <amu@sakai.pro>
2021-05-08 15:34:54 +0900

    長さ判別

--- No2/StudentIdValidator.java
+++ No2/StudentIdValidator.java
@@ -1,9 +1,19 @@
 public class StudentIdValidator{
   void run(String[] args){
+    System.out.println(args[0]);  ///test
+    validate(args[0]);
+  }
+
+  void validate(String id){
+    Integer length1;
+    length1 = id.length();
+    if(length1 != 6){
+      System.exit(0);
+    }
+    System.out.println(length1); ///test
 
   }
 
-  // mainメソッドは省略．
   public static void main(String[] args){
     StudentIdValidator application = new StudentIdValidator();
     application.run(args);
