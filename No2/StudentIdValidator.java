d301bca43f859db56ad1208e455123480593685e
sakai <amu@sakai.pro>
2021-05-08 15:15:04 +0900

    雛形追加

---
+++ No2/StudentIdValidator.java
@@ -0,0 +1,11 @@
+public class StudentIdValidator{
+  void run(String[] args){
+
+  }
+
+  // mainメソッドは省略．
+  public static void main(String[] args){
+    StudentIdValidator application = new StudentIdValidator();
+    application.run(args);
+  }
+}
