095f511eeb424f69f471b002bcd4784728fcbe40
sakai-desk <amu@sakai.pro>
2021-05-08 15:15:47 +0900

    Auto stash before merge of "StudentIdValidator" and "origin/StudentIdValidator"

--- No2/StudentIdValidator.java
+++ No2/StudentIdValidator.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 public class StudentIdValidator{
   void run(String[] args){
 
@@ -9,3 +10,16 @@ public class StudentIdValidator{
     application.run(args);
   }
 }
+=======
+public class StudentIdValidator{
+  void run(String[] args){
+    // ここで，printArray を呼び出し，"before"の一行を出力する．
+    printArray(args);
+    // argsの内容をソートするため，Arrays.sortメソッドを呼び出す．
+    Arrays.sort(args); // <= args がソート済みになる．
+    // ここで，printArray を呼び出し，"after"の一行を出力する．
+    printArray(args);
+  }
+
+}
+>>>>>>> Stashed changes
