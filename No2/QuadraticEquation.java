3e1ee725417821e87770791eeebfc7f2e0fb418b
sakai-desk <amu@sakai.pro>
2021-05-09 14:08:42 +0900

    雛形

---
+++ No2/QuadraticEquation.java
@@ -0,0 +1,10 @@
+public class QuadraticEquation{
+  void run(String[] args){
+    validate(args[0]);
+  }
+
+  public static void main(String[] args){
+    QuadraticEquation application = new QuadraticEquation();
+    application.run(args);
+  }
+}
