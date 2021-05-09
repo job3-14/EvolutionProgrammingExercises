8391d08fc040f4c7272b0be15fbdba09a75bab2a
sakai-desk <amu@sakai.pro>
2021-05-09 14:42:28 +0900

    入力追加

--- No2/QuadraticEquation.java
+++ No2/QuadraticEquation.java
@@ -1,6 +1,13 @@
 public class QuadraticEquation{
   void run(String[] args){
-    validate(args[0]);
+    Double inputA = Double.valueOf(args[0]);
+    Double inputB = Double.valueOf(args[1]);
+    Double inputC = Double.valueOf(args[2]);
+    discriminant(inputA, inputB, inputC);
+  }
+
+  void discriminant(Double inputA, Double inputB, Double inputC){
+    System.out.println(inputA);
   }
 
   public static void main(String[] args){
