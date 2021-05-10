b5609585957183f8af3ecc45bef038c23d676ac9
sakai-desk <amu@sakai.pro>
2021-05-10 13:47:23 +0900

    完成

--- No2/MonteCarloPi.java
+++ No2/MonteCarloPi.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class MonteCarloPi{
   void run(String[] args){
     Integer inputNumber;
@@ -6,6 +7,28 @@ public class MonteCarloPi{
     }else{
       inputNumber = 1000;
     }
+    calculation(inputNumber);
+  }
+
+  void calculation(Integer number){
+    Double randomX, randomY, distans, result;
+    Integer i, count=0;
+    //乱数生成
+    Random rnd = new Random();
+
+    for(i=0;i<number;i++){
+      randomX = rnd.nextDouble();
+      randomY = rnd.nextDouble();
+      distans = Math.sqrt(randomX * randomX + randomY * randomY);
+      if(distans <= 1){
+        count++;
+      }
+    }
+    result = (double)count / number * 4.0;
+    System.out.printf("pi = %f\n",result);
+
+
+
   }
 
   public static void main(String[] args){
