27adf4cc081ddbfdc2ffccf2efa2c5fcb2aa7fcb
sakai-desk <amu@sakai.pro>
2021-05-10 18:43:21 +0900

    雛形

---
+++ No2/TrapezoidalRulePi.java
@@ -0,0 +1,11 @@
+public class TrapezoidalRulePi{
+  void run(String[] args){
+
+  }
+
+
+  public static void main(String[] args){
+    TrapezoidalRulePi application = new TrapezoidalRulePi();
+    application.run(args);
+  }
+}
