bb204d7e2a864d9f14457f724430a2c51e88115c
sakai-desk <amu@sakai.pro>
2021-05-17 18:45:12 +0900

    完成

--- No3/Factorizer.java
+++ No3/Factorizer.java
@@ -1,39 +1,46 @@
 import java.util.ArrayList;
 public class Factorizer{
   void run(String[] args){
-    Integer number,count=1;
+    Integer i,j,number,count=1;
     ArrayList<Integer> primesList = new ArrayList<>();
-    if(args.length == 1){
-      number = Integer.valueOf(args[0]);
-    }else{
-      number = 200;
-    }
-    primeFactorization(number);
-    }
-
-    ArrayList<Integer> primeFactorization(Integer number){
-      Integer i, primeTmp, numberTmp=number ,count=1;
-      Boolean status=true;
-      ArrayList<Integer> answerList = new ArrayList<>();
-      Primes primes = new Primes();
-      ArrayList<Integer> primeList = primes.calculation(number);
-      while(status){
-        for(i=1;i<=numberTmp;i++){
-          primeTmp = primeList.get(i);
-          if(numberTmp % primeTmp == 0){
-            numberTmp = numberTmp / primeTmp;
-            answerList.add(primeTmp);
-            count *= primeTmp;
-            break;
-          }
+    ArrayList<Integer> answer = new ArrayList<>();
+    for(i=0;i<args.length;i++){
+      number = Integer.valueOf(args[i]);
+      answer = primeFactorization(number);
+      System.out.printf("%d: ",number);
+      for(j=0; j<answer.size(); j++){
+        System.out.printf("%d ",answer.get(j));
+        if(j != answer.size()-1){
+          System.out.printf("x ");
+        }else{
+          System.out.printf("\n");
         }
-        if(count>=number){
-          status = false;
-        }
-
       }
-      return answerList;
     }
+  }
+
+  ArrayList<Integer> primeFactorization(Integer number){
+    Integer i, primeTmp, numberTmp=number ,count=1;
+    Boolean status=true;
+    ArrayList<Integer> answerList = new ArrayList<>();
+    Primes primes = new Primes();
+    ArrayList<Integer> primeList = primes.calculation(number);
+    while(status){
+      for(i=1;i<=numberTmp;i++){
+        primeTmp = primeList.get(i);
+        if(numberTmp % primeTmp == 0){
+          numberTmp = numberTmp / primeTmp;
+          answerList.add(primeTmp);
+          count *= primeTmp;
+          break;
+        }
+      }
+      if(count>=number){
+        status = false;
+      }
+    }
+    return answerList;
+  }
 
 
   public static void main(String[] args){
