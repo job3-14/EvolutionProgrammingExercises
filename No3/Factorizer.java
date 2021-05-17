d852d0f6ad5597a0960e1a104d5aa0b6aa01fdbf
sakai-desk <amu@sakai.pro>
2021-05-17 18:37:03 +0900

    計算結果

--- No3/Factorizer.java
+++ No3/Factorizer.java
@@ -1,46 +1,43 @@
 import java.util.ArrayList;
-public class Primes{
+public class Factorizer{
   void run(String[] args){
-    Integer number,i,count=1;
+    Integer number,count=1;
     ArrayList<Integer> primesList = new ArrayList<>();
     if(args.length == 1){
       number = Integer.valueOf(args[0]);
     }else{
       number = 200;
     }
-    primesList = calculation(number);
-  }
-
-  ArrayList<Integer> calculation(Integer number){
-    Integer i,j,k,tmp,lastPrime, count;
-    ArrayList<Boolean> list = new ArrayList<>();
-    ArrayList<Integer> primesList = new ArrayList<>();
-    primesList.add(1);
-    for(i=0;i<=number;i++){
-      list.add(true);
+    primeFactorization(number);
     }
-    for(i=2;i<=number;i++){
-      lastPrime = primesList.get(primesList.size()-1);
-      for(j=lastPrime+1;j<=number;j++){
-        if(list.get(j)==true){
-          primesList.add(j);
-          for(k=j;k<=number;k++){ //素数の倍数除外
-            if(k%j == 0){
-              list.set(k, false);
-            }
+
+    ArrayList<Integer> primeFactorization(Integer number){
+      Integer i, primeTmp, numberTmp=number ,count=1;
+      Boolean status=true;
+      ArrayList<Integer> answerList = new ArrayList<>();
+      Primes primes = new Primes();
+      ArrayList<Integer> primeList = primes.calculation(number);
+      while(status){
+        for(i=1;i<=numberTmp;i++){
+          primeTmp = primeList.get(i);
+          if(numberTmp % primeTmp == 0){
+            numberTmp = numberTmp / primeTmp;
+            answerList.add(primeTmp);
+            count *= primeTmp;
+            break;
           }
-          break;
         }
+        if(count>=number){
+          status = false;
+        }
+
       }
+      return answerList;
     }
-    return primesList;
-  }
-
-
 
 
   public static void main(String[] args){
-    Primes application = new Primes();
+    Factorizer application = new Factorizer();
     application.run(args);
   }
 }
