476c2884cbf99cd67912b06864be4984147c0c26
sakai <amu@sakai.pro>
2021-06-07 14:55:33 +0900

    完成

--- No6/DrawShapes2.java
+++ No6/DrawShapes2.java
@@ -1,17 +1,18 @@
 import java.awt.Color;
 
-public class DrawOval{
+public class DrawShapes2{
   void run(){
     EZ.initialize(400, 400); // 画面の大きさを決める．
-    // 円を描く．(中心座標x, y, 幅，高さ，色，塗りつぶし)
-    EZCircle circle1 = EZ.addCircle(
-        100, 100, 200, 200, Color.BLUE, true);
-    EZCircle circle2 = EZ.addCircle(
-        200, 200, 200, 200, Color.RED,  false);
+    EZRectangle rectangle1 = EZ.addRectangle(200, 200, 200, 200, Color.BLUE, true); //四角を書く
+    EZCircle circle1 = EZ.addCircle(200, 200, 200, 200, Color.WHITE, true); //円を書く
+    EZLine line1 = EZ.addLine(200, 50, 50, 200, Color.RED);
+    EZLine line2 = EZ.addLine(50, 200, 200, 350, Color.GREEN);
+    EZLine line3 = EZ.addLine(200, 350, 350, 200, Color.LIGHT_GRAY);
+    EZLine line4 = EZ.addLine(350, 200, 200, 50, Color.ORANGE);
   }
   // mainメソッドは省略．
   public static void main(String[] args){
-    DrawOval application = new DrawOval();
+    DrawShapes2 application = new DrawShapes2();
     application.run();
   }
 }
