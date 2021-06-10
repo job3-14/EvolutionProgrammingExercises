d2907c90eff166bfe7a635f4728f919250b0d801
sakai <amu@sakai.pro>
2021-06-10 09:58:46 +0900

    完成

---
+++ No6/test7.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+import java.lang.Math;
+
+public class Reversi{
+  void run(String[] args) throws InterruptedException{
+    EZ.initialize(420, 420);
+
+  }
+
+
+
+  public static void main(String[] args) throws InterruptedException{
+    Reversi application = new Reversi();
+    application.run(args);
+  }
+}
