0ff712bd5db1062a967dbebbc456ba7fe2224551
sakai <amu@sakai.pro>
2021-06-15 09:52:01 +0900

    完成

--- No8/MakeDirectory.java
+++ No8/MakeDirectory.java
@@ -2,7 +2,12 @@ import java.io.File;
 
 public class MakeDirectory{
   void run(String[] args){
-    
+    File dir = new File(args[0]);
+    makedir(dir);
+  }
+
+  void makedir(File dir){
+    dir.mkdir();
   }
 
   public static void main(String[] args){
