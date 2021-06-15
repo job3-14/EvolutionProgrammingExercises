23df7e4dbe8d9205a4a46e1bf70c4985e2d08d8a
sakai <amu@sakai.pro>
2021-06-15 09:14:49 +0900

    雛形

---
+++ No8/MakeDirectory.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class MakeDirectory{
+  void run(String[] args){
+    
+  }
+
+  public static void main(String[] args){
+    MakeDirectory application = new MakeDirectory();
+    application.run(args);
+  }
+}
