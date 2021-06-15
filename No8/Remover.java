f4629961f46439a2ae7dade65876ed514b5589df
sakai <amu@sakai.pro>
2021-06-15 10:04:18 +0900

    処理追加

--- No8/Remover.java
+++ No8/Remover.java
@@ -4,10 +4,13 @@ public class Remover{
   void run(String[] args){
     for(Integer i=0; i < args.length; i++){
       File dir = new File(args[i]);
-      makedir(dir);
+      delete(dir);
     }
   }
 
+  void delete(File dir){
+    dir.delete();
+  }
 
   public static void main(String[] args){
     Remover application = new Remover();
